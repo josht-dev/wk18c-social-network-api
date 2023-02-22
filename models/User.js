182e38b2812c812b9bd7d275693c06b729b70fbf
Joshua Taylor <toasterrage@gmail.com>
2023-02-22 01:31:56 -0700

    Updated user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,22 @@
 // *****Import Modules*****
 const { Schema, model } = require('mongoose');
 
-
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please use a valid email address.']
+    }
+  }
+);
 
 
 
