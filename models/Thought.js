c1b9378487070d96ecf04a15b66a53862f1fc673
Joshua Taylor <toasterrage@gmail.com>
2023-02-23 12:22:05 -0700

    Added Thought model

---
+++ models/Thought.js
@@ -0,0 +1,21 @@
+// *****Import Modules*****
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }
+)
+
+
+
+module.exports = Thought;
