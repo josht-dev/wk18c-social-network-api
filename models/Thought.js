3c42a9ffe5bd1e8ee4eb902e10560deffb4deaeb
Joshua Taylor <toasterrage@gmail.com>
2023-02-26 12:20:12 -0700

    Added thought Model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,6 @@
 // *****Import Modules*****
 const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
 
 // Model Schema
 const thoughtSchema = new Schema(
@@ -12,10 +13,24 @@ const thoughtSchema = new Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now
-    }
+      default: Date.now,
+      get: (date) => {
+        // EX: jun 10, 2020 at 01:38pm
+        return date.toLocaleString('en-US', { dateStyle: 'long', timeStyle: 'medium' });
+      }
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [Reaction]
   }
 )
+
+// Virtual property that returns reactions count
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+})
 
 // Initialize model
 const Thought = model('thought', thoughtSchema);
