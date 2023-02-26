423679a3f1b13c512f8ae9250bd2008ff25b15ac
Joshua Taylor <toasterrage@gmail.com>
2023-02-26 16:07:36 -0700

    Added api routes index file

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+// *****Import Modules*****
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
