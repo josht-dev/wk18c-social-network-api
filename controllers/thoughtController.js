72b68c6ccde7de36bb61f7d3adc479e40d8e8a96
Joshua Taylor <toasterrage@gmail.com>
2023-02-26 13:44:44 -0700

    Updated controllers with router function skeletons

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,15 +4,22 @@ const Thought = require('../models/Thought');
 module.exports = {
   // ***** Route - /api/thoughts *****
   // TODO - Get all thoughts
+  getThoughts(req, res) {},
   /* TODO - Post/create a new thought,
     push created thought's _id to associated user's thoughts array field */
+  createThought(req, res) {},
   // ***** Route - /api/thoughts/thoughtId *****
   // TODO - Get a single thought by its _id
+  getOneThought(req, res) {},
   // TODO - Put/update a though by its _id
+  updateThought(req, res) {},
   // TODO - Delete a thought by its _id
+  deleteThought(req, res) {},
   // ***** Route - /api/thoughts/:thoughtId/reactions *****
   /* TODO - Post/create a reaction,
     store in a single thought's reactions array field */
-  // ***** Route - /api/thoughts/:thoughtId/reactions/:reactionId *****
+  createReaction(req, res) {},
+    // ***** Route - /api/thoughts/:thoughtId/reactions/:reactionId *****
   // TODO - Delete a reaction by the reaction's reactionId
+  deleteReaction(req, res) {}
 }
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,14 +4,21 @@ const User = require('../models/User');
 module.exports = {
   // *****Route - /api/users *****
   // TODO - Get all users
+  getUsers(req, res) {},
   // TODO -Post/create a new user
+  createUser(req, res) {},
   // ***** Route - /api/users/:userId *****
   /* TODO - Get a single user by its _id,
     populate thought and friend data */
+  getOneUser(req, res) {},
   // TODO - Put/update a user by its _id
+  updateUser(req, res) {},
   // TODO - Delete a user by its _id
   // TODO - BONUS - Remove user's associated thoughts when deleted
+  deleteUser(req, res) {},
   // ***** Route - /api/users/:userId/friends/:friendId *****
   // TODO - Post/create to add a new friend to user's friend list
+  createFriend(req, res) {},
   // TODO - Delete friend from user's friend list
+  deleteFriend(req, res) {}
 }
