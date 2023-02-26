1bf3b7d5a95540c6ee69e97313bc598c847140ae
Joshua Taylor <toasterrage@gmail.com>
2023-02-26 13:35:16 -0700

    Updated controller comments

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,17 +2,17 @@
 const Thought = require('../models/Thought');
 
 module.exports = {
-  // Route - /api/thoughts
+  // ***** Route - /api/thoughts *****
   // TODO - Get all thoughts
   /* TODO - Post/create a new thought,
     push created thought's _id to associated user's thoughts array field */
-  // Route - /api/thoughts/thoughtId
+  // ***** Route - /api/thoughts/thoughtId *****
   // TODO - Get a single thought by its _id
   // TODO - Put/update a though by its _id
   // TODO - Delete a thought by its _id
-  // Route - /api/thoughts/:thoughtId/reactions
+  // ***** Route - /api/thoughts/:thoughtId/reactions *****
   /* TODO - Post/create a reaction,
     store in a single thought's reactions array field */
-    // Route - /api/thoughts/:thoughtId/reactions/:reactionId
+  // ***** Route - /api/thoughts/:thoughtId/reactions/:reactionId *****
   // TODO - Delete a reaction by the reaction's reactionId
 }
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,16 +2,16 @@
 const User = require('../models/User');
 
 module.exports = {
-  // Route - /api/users
+  // *****Route - /api/users *****
   // TODO - Get all users
   // TODO -Post/create a new user
-  // Route - /api/users/:userId
+  // ***** Route - /api/users/:userId *****
   /* TODO - Get a single user by its _id,
     populate thought and friend data */
   // TODO - Put/update a user by its _id
   // TODO - Delete a user by its _id
   // TODO - BONUS - Remove user's associated thoughts when deleted
-  // Route - /api/users/:userId/friends/:friendId
+  // ***** Route - /api/users/:userId/friends/:friendId *****
   // TODO - Post/create to add a new friend to user's friend list
   // TODO - Delete friend from user's friend list
 }
