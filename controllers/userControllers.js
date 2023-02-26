619df8c69980ba29f6783237382126048feeab00
Joshua Taylor <toasterrage@gmail.com>
2023-02-26 13:33:03 -0700

    Updated comments

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,10 +4,10 @@ const User = require('../models/User');
 module.exports = {
   // Route - /api/users
   // TODO - Get all users
+  // TODO -Post/create a new user
   // Route - /api/users/:userId
   /* TODO - Get a single user by its _id,
     populate thought and friend data */
-  // TODO -Post/create a new user
   // TODO - Put/update a user by its _id
   // TODO - Delete a user by its _id
   // TODO - BONUS - Remove user's associated thoughts when deleted
