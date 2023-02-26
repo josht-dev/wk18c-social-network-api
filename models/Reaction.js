a25b333d86932e5676e07bfa4ef72eae0dbe6a11
Joshua Taylor <toasterrage@gmail.com>
2023-02-26 12:26:36 -0700

    Added Response schema

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+// *****Import Modules*****
+const { Schema, Types } = require('mongoose');
+
+// Schema only
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        // EX: jun 10, 2020 at 01:38pm
+        return date.toLocaleString('en-US', { dateStyle: 'long', timeStyle: 'medium' });
+      }
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+module.exports = reactionSchema;
