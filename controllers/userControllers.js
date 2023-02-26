c483b9d6eb5ba30d621bea073882d164ca331c9b
Joshua Taylor <toasterrage@gmail.com>
2023-02-26 13:17:51 -0700

    Added userController

---
+++ controllers/userControllers.js
@@ -0,0 +1,17 @@
+// *****Import Modules*****
+const User = require('../models/User');
+
+module.exports = {
+  // Route - /api/users
+  // TODO - Get all users
+  // Route - /api/users/:userId
+  /* TODO - Get a single user by its _id,
+    populate thought and friend data */
+  // TODO -Post/create a new user
+  // TODO - Put/update a user by its _id
+  // TODO - Delete a user by its _id
+  // TODO - BONUS - Remove user's associated thoughts when deleted
+  // Route - /api/users/:userId/friends/:friendId
+  // TODO - Post/create to add a new friend to user's friend list
+  // TODO - Delete friend from user's friend list
+}
