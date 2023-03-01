c711479dbc64bd1139be15d93d539bbd8bb6f59a
Joshua Taylor <toasterrage@gmail.com>
2023-02-28 20:53:25 -0700

    Updated thought create to give thought id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -14,8 +14,24 @@ module.exports = {
     push created thought's _id to associated user's thoughts array field */
   createThought(req, res) {
     Thought.create(req.body)
-      .then((thought) => res.json(thought))
-      .catch((err) => res.status(500).json(err));
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { username: req.body.username },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+            message: 'Thought created, but user with that id is lost!',
+          })
+          : res.json({ message: `The thought, ID: ${user.thoughts[user.thoughts.length - 1]} has been formed!`})
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
   // ***** Route - /api/thoughts/thoughtId *****
   // TODO - Get a single thought by its _id
