00732427c3b784c416abc672be9d5d2eddf0ab8d
Joshua Taylor <toasterrage@gmail.com>
2023-02-28 21:27:25 -0700

    Updated comments

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,14 +3,14 @@ const { Thought, User } = require('../models');
 
 module.exports = {
   // ***** Route - /api/thoughts *****
-  // TODO - Get all thoughts
+  // Get all thoughts
   getThoughts(req, res) {
     Thought.find()
       .select('-__v')
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
-  /* TODO - Post/create a new thought,
+  /* Post/create a new thought,
     push created thought's _id to associated user's thoughts array field */
   createThought(req, res) {
     Thought.create(req.body)
@@ -34,7 +34,7 @@ module.exports = {
       });
   },
   // ***** Route - /api/thoughts/thoughtId *****
-  // TODO - Get a single thought by its _id
+  // Get a single thought by its _id
   getOneThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
       .select('-__v')
@@ -45,7 +45,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  // TODO - Put/update a though by its _id
+  // Put/update a though by its _id
   updateThought(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
@@ -62,7 +62,7 @@ module.exports = {
         res.status(500).json(err);
       });
   },
-  // TODO - Delete a thought by its _id
+  // Delete a thought by its _id
   deleteThought(req, res) {
     Thought.findOneAndRemove({ _id: req.params.thoughtId })
       .then((thought) =>
@@ -84,7 +84,7 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
   // ***** Route - /api/thoughts/:thoughtId/reactions *****
-  /* TODO - Post/create a reaction,
+  /* Post/create a reaction,
     store in a single thought's reactions array field */
   createReaction(req, res) {
     Thought.findOneAndUpdate(
@@ -100,7 +100,7 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
     // ***** Route - /api/thoughts/:thoughtId/reactions/:reactionId *****
-  // TODO - Delete a reaction by the reaction's reactionId
+  // Delete a reaction by the reaction's reactionId
   deleteReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
